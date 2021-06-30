c64efcd36706b3c222345cf839a3c66541aa5a67
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-06-30 00:53:48 -0700

    initial commit

---
+++ FactoryPatternExercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FactoryPatternExercise1
+{
+    class Program
+    {
+       
+
+        static void Main(string[] args)
+        {
+            int wheels;
+            bool input = false;
+            do
+
+            {
+                Console.WriteLine("Enter the amount of tires for the vehicle you wish to build:");
+
+                input = int.TryParse(Console.ReadLine(), out wheels);
+            }
+            while (input == false);
+            var vehicle = VehicleFactory.GetVehicle(wheels);
+            vehicle.Drive();
+
+        }
+    }
+}
